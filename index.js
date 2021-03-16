01992384acab02eecaf89e6cf372f17bc470ee5f
Mario Tutek <m.tutek@gmail.com>
2021-03-16 19:27:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 function add(number) {
   number+=10
 }
+
+function subtract(number) {
+  number-=5
+}
+
+function multiply(number) {
+  number*=2
+}
+function divide(number) {
+  number/=4
+}
+function add2(a,b) {
+  return(a+b)
+}
+function subtract2(a,b) {
+  return(b-a)
+}
