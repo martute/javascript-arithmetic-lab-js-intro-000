4c7846671646167416c3ea50d7206c7ae8968540
Mario Tutek <m.tutek@gmail.com>
2021-03-16 20:17:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ function decrement(n) {
   return(n-=1)
 }
 function makeInt(string) {
-  parseInt(string, 10);
-  console.log(string);
+  parseInt(n, 10);
+  console.log(n);
 }
 
 function preserveDecimal(n) {
